6dacd1d546602bc2dc7e9952bb88ed6b3dcd7c36
dariasteam <dariasteam94@gmail.com>
2017-12-24 17:40:05 +0000

    Añade espejado rotaciones en múltiplos de 90º

---
+++ src/plugins/Rotate_180/connector_plugin.cpp
@@ -0,0 +1,28 @@
+#include <QtWidgets>
+
+#include "connector_plugin.h"
+
+conector_plugin::conector_plugin () {  
+  meta_info = {
+  /* name            */  "Rotar 180º",
+  /* category        */  "Editar",
+  /* can_work_no_gui */  true,
+  /* is_no_gui       */  true,
+  /* can preview     */  true,
+  /* require_lut     */  false,
+  /* require_canvas  */  false,
+  /* can overwrite   */  true,
+  };
+}
+
+void conector_plugin::close() {  
+
+}
+
+void conector_plugin::instance() {  
+  if (view == nullptr && mdl == nullptr && ctrller == nullptr) {        
+    view = new menu();        
+    mdl = new model ();
+    ctrller = new controller(view, mdl);        
+  }
+}
