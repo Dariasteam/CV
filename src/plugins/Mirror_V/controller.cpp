f3aee755585636bc4dbfc37f12136488d1b445fa
dariasteam <dariasteam94@gmail.com>
2017-12-24 16:13:22 +0000

    Añade espejado vertical y horizontal

---
+++ src/plugins/Mirror_V/controller.cpp
@@ -0,0 +1,29 @@
+#include "controller.h"
+
+#include <iostream>
+
+controller::controller(QWidget *mn, PluginModel *mdl) :
+  plugin_controller (mn, mdl)
+{}
+
+bool controller::operator () (picture* image, LUT* lut, canvas_image_label* canvas) {
+
+  mdl->set_image(image);
+
+  picture* aux = image->make_copy();
+
+  unsigned width  = image->get_image()->size().width();
+  unsigned height = image->get_image()->size().height();
+  QImage* img = image->get_image();
+
+  aux->each_pixel_modificator_with_index([&](QColor color, unsigned i, unsigned j) -> QColor {
+    return (img->pixelColor(i, height - j));
+  });
+
+  image->restore_from(aux);
+
+  emit update_inform();
+
+  return true;
+}
+
